7dfc5cfa75ba167c7c0b811c45d3f7c2c1538072
AkhmalN <cocwar427@gmail.com>
2024-12-31 03:02:54 +0700

    rename project repository

--- FE-Sales-management/src/types/invoice.ts
+++
@@ -1,7 +0,0 @@
-export type INVOICE_TYPE = {
-  invoice: string;
-  name: string;
-  email: string;
-  date: string;
-  status_invoice: string;
-};
--- FE-Sales-management/src/types/top_selling_products.ts
+++
@@ -1,6 +0,0 @@
-export type TOP_SELLING_PRODUCTS_TYPE = {
-  image: string;
-  product_name: string;
-  rating: number;
-  price: string;
-};
